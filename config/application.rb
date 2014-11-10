563088d972d6364297e7f89e433d7a58e78ad545
tsuka54 <y_tsukagoshi@zenk.co.jp>
2014-11-10 18:31:20 +0900

    ルート追加 ライセンス宣言追加 等々

--- config/application.rb
+++ config/application.rb
@@ -15,16 +15,15 @@ Bundler.require(*Rails.groups)
 
 module Fwm
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.time_zone = 'Tokyo'
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb.yml}').to_s]
+    config.i18n.default_locale = :ja
+    config.generators do |gen|
+      gen.helper false
+      gen.assets false
+      gen.test_framework :rspec
+      gen.controller_specs false
+      gen.view_specs false
+    end
   end
 end
