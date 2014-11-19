773c9df4c6961dbe6771140fa6d60d4b1cd6977d
tsuka54 <y_tsukagoshi@zenk.co.jp>
2014-11-19 18:38:19 +0900

    レイアウトを管理者用と作業者用に分割

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  layout :getLayout
+
+  private
+  def getLayout
+    if params[:controller].match( %r{\A(management|staff)/} )
+      Regexp.last_match[1]
+    else
+      'staff'
+    end
+  end
 end
